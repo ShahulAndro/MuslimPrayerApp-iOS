c174772495a0d084ecff4c3b8050adba8b5bca57
Shahul Hameed Shaik <tshahul@hlbb.hongleong.com.my>
2022-04-28 01:49:39 +0800

    Updated extensions

--- MuslimPrayerApp/Shared/Extensions/NSLayoutConstraint+Extension.swift
+++ MuslimPrayerApp/Shared/Extensions/NSLayoutConstraint+Extension.swift
@@ -12,14 +12,16 @@ extension NSLayoutConstraint {
     static func setMultiplier(_ multiplier: CGFloat, of constraint: inout NSLayoutConstraint) {
         NSLayoutConstraint.deactivate([constraint])
 
-        let newConstraint = NSLayoutConstraint(item: constraint.firstItem, attribute: constraint.firstAttribute, relatedBy: constraint.relation, toItem: constraint.secondItem, attribute: constraint.secondAttribute, multiplier: multiplier, constant: constraint.constant)
+        if let firstItem = constraint.firstItem {
+            let newConstraint = NSLayoutConstraint(item: firstItem, attribute: constraint.firstAttribute, relatedBy: constraint.relation, toItem: constraint.secondItem, attribute: constraint.secondAttribute, multiplier: multiplier, constant: constraint.constant)
 
-        newConstraint.priority = constraint.priority
-        newConstraint.shouldBeArchived = constraint.shouldBeArchived
-        newConstraint.identifier = constraint.identifier
+            newConstraint.priority = constraint.priority
+            newConstraint.shouldBeArchived = constraint.shouldBeArchived
+            newConstraint.identifier = constraint.identifier
 
-        NSLayoutConstraint.activate([newConstraint])
-        constraint = newConstraint
+            NSLayoutConstraint.activate([newConstraint])
+            constraint = newConstraint
+        }
     }
 
 }
